b4727d9dc00d3f9d6ed15cc959dbe8243ff01806
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2024-08-23 15:14:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        ##Your code here
+        #Return true or false
+        pr={0:-1}
+        total=0
+        for i in range(len(arr)):
+            total=total+arr[i]
+            if total in pr:
+                return True
+            if total not in pr:
+                pr[total]=i
+        return False
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
