b89629479569e23fbc7c32b3f067aea2861a7c8b
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2024-09-06 10:24:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -4,13 +4,15 @@ class Solution:
     ##Complete this function
     #Function to find the sum of contiguous subarray with maximum sum.
     def maxSubArraySum(self,arr):
-        mn=arr[0]
-        ms=arr[0]
-        for i in range(1,len(arr)):
-            mn = max(arr[i],mn+arr[i])
-            ms=max(ms,mn)
-        return ms
-        ##Your code here
+        maxe=-99999999
+        ma=-99999999
+        maxs=-99999999
+        for i in arr:
+            ma=maxe+i
+            maxe=max(ma,i)
+            maxs=max(maxe,maxs)
+        return maxs
+            
 
 
 #{ 
